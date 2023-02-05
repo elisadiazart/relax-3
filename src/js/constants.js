9f9c6d8dd918178fdb650efa02f17e1574c30d6b
Elisa Diaz <elisadiazartcontact@gmail.com>
2023-02-05 17:34:30 -0100

    05.02.23

---
+++ src/js/constants.js
@@ -0,0 +1,5 @@
+export const DESTINY_ITEMS = {
+  names: ['Elisa', 'Alejandro', 'Daniel'],
+  numbers: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+  things: ['besos', 'capones', 'abrazos']
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,40 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
+import { DESTINY_ITEMS } from './constants';
 
-sayHello();
+const buttons = document.getElementById('buttons')
+const text = document.getElementById('destiny-title')
+
+let counterName= 0
+let counterNumber= 0
+
+const setName = () => {
+    text.textContent = `${DESTINY_ITEMS.names[counterName]} se merece `
+    if(counterName > DESTINY_ITEMS.names.length -2){
+         counterName=0
+    }
+    else if(counterName < 0) {
+        counterName = DESTINY_ITEMS.names.length -2
+    }else counterName++
+}
+
+const setNumber = () => {
+    text.textContent += DESTINY_ITEMS.numbers[counterNumber]
+    console.log(counterNumber)
+    if(counterNumber > DESTINY_ITEMS.numbers.length -2){
+         counterNumber=0
+    }
+    else if(counterNumber < 0) {
+        counterNumber = DESTINY_ITEMS.numbers.length -2
+    }else counterNumber++
+}
+
+
+
+buttons.addEventListener('click', e => {
+    text.textContent = ''
+    if(e.target.dataset.item === 'name')setName()
+    else if (e.target.dataset.item === 'number') setNumber()
+})
+
+console.log(DESTINY_ITEMS.numbers)
