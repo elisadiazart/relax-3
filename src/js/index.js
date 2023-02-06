c47046318ab6f32c261d804657467ea39109aa18
Elisa <elisadiazartcontact@gmail.com>
2023-02-06 23:05:20 +0100

    06.02.23

--- src/js/index.js
+++ src/js/index.js
@@ -7,26 +7,32 @@ const text = document.getElementById('destiny-title')
 
 let counterName= 0
 let counterNumber= 0
+let arrayPhrase=''
 
 const setName = () => {
-    text.textContent = `${DESTINY_ITEMS.names[counterName]} se merece `
     if(counterName > DESTINY_ITEMS.names.length -2){
          counterName=0
     }
     else if(counterName < 0) {
         counterName = DESTINY_ITEMS.names.length -2
     }else counterName++
+    console.log(DESTINY_ITEMS.names[counterName])
 }
 
 const setNumber = () => {
-    text.textContent += DESTINY_ITEMS.numbers[counterNumber]
-    console.log(counterNumber)
     if(counterNumber > DESTINY_ITEMS.numbers.length -2){
          counterNumber=0
     }
     else if(counterNumber < 0) {
         counterNumber = DESTINY_ITEMS.numbers.length -2
     }else counterNumber++
+    console.log(DESTINY_ITEMS.numbers[counterNumber])
+}
+
+const printArray = (name, number, thing) =>{
+    arrayPhrase=''
+    console.log(arrayPhrase)
+    text.textContent= arrayPhrase
 }
 
 
